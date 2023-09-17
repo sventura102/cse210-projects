0b24a57f563f5717c8f2fcd097e1555441c75d6c
sventura102 <sventura102@gmail.com>
2023-09-16 20:18:16 -0700

    letter grade

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,35 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your score?");
+        string score = Console.ReadLine();
+        int number = int.Parse(score);
+
+        string letter = "";
+
+        if (number >= 90 || number <= 100) {
+            letter = "A";
+        }
+        else if (number >= 80 || number <= 89) {
+            letter = "B";
+        }
+        else if (number >=70 || number <= 79) {
+            letter = "C";
+        }
+        else if( number >= 60 || number <= 69) {
+            letter = "D";
+        }
+        else {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}.");
+
+        if (number > 70) {
+            Console.WriteLine("Congratulations! You passed!");
+        }
+        else {
+            Console.WriteLine("That's ok! Try your best next time!");
+        }
     }
 }
