ccee0404771f09c675a87acf26dc4f0b07b9c645
sventura102 <sventura102@gmail.com>
2023-09-21 21:35:01 -0700

    resume

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Patient Associate";
+        job1._company = "Shepherd Eye Center";
+        job1._startYear = 2019;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Program Supervisor";
+        job2._company = "Safekey";
+        job2._startYear = 2023;
+        job2._endYear = 2023;
+
+        Resume resume = new Resume();
+        resume._name = "Stephanie Ventura";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Display();
     }
 }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -7,3 +7,26 @@ class Program
         Console.WriteLine("Hello Develop02 World!");
     }
 }
+
+// 1. Options class
+// - display 5 questions
+// - execute task based on user option
+// - variables: string variable, convert int
+//
+// 2. randomPrompt class
+// - If question already asked, do not repeat
+// - Display questions
+// - variables: list prompts, loop, display
+//
+// 3. Entry
+// - random prompt displayed
+// - user entry accounted for such as input and time
+// - saved to the journal
+// - variables: string entryText (user inputs data), display option, string date, string promptText (displays prompt)
+//
+// 4. Journal
+// - saved to a file (the file itself) -> .csv file
+// - able to load/save
+// - display entries
+// - add entry
+// - variables: list journal, list entry
