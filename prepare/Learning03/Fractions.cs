ab3777d238d1179b1fa45a933e264543cd7c9d8b
sventura102 <sventura102@gmail.com>
2023-09-30 18:53:19 -0700

    finished fractions

---
+++ prepare/Learning03/Fractions.cs
@@ -0,0 +1,23 @@
+public class Fractions {
+    private int _top;
+    private int _bottom;
+
+    public Fractions() {
+        _top = 1;
+        _bottom = 1;
+    }
+    public Fractions(int wholeNumber) {
+        _top = 1;
+        _bottom = wholeNumber;
+    }
+    public Fractions(int bottom, int top) {
+        _top = top;
+        _bottom = bottom;
+    }
+    public string GetFractionString() {
+
+    }
+    public double GetDecimalValue() {
+        
+    }
+}
