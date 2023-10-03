b9a9997ed4d7cfb17aac88fd9b916cd340276d22
sventura102 <sventura102@gmail.com>
2023-10-02 21:40:35 -0700

    completed Promp Generator w/ Random selections

--- prove/Develop02/PromptGen.cs
+++ prove/Develop02/PromptGen.cs
@@ -1,13 +1,17 @@
 using System;
+using System.Security.Cryptography;
 // 2. randomPrompt class
 // - If question already asked, do not repeat
 // - Display questions
 // - variables: list prompts, loop, display
 public class PromptGen {
-    public List<string> _prompts = public string GetRandomPrompt();
+    public List<string> _prompts = new List<string> {"List three things you're grateful for today.", "Write about a song and what it made you feel.", "What color do you feel like and why?"};
 
-    public string GetRandomPrompt()
-        {
-            return "List three things you're grateful for today.";
-        }
+    public string GetRandomPrompt() {
+        Random randNum = new Random();
+        int RandomPos = randNum.Next(_prompts.Count);
+        
+        string randomPrompt = _prompts[RandomPos];
+        return randomPrompt;
+    }
     }
