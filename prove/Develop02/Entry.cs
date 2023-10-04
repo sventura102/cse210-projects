b9984f9ef26b0c6066778cbaec68103969d2b90f
sventura102 <sventura102@gmail.com>
2023-10-03 18:31:38 -0700

    created a constructor for entry class so it can be called externally

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -7,9 +7,25 @@ public class Entry {
     public string _date;
     public string _promptText;
     public string _entryText;
+    // A constructor is a special function which each class may contain,
+    // and in order for a function to be a constructor, its name must 
+    // match the name of the class in which it resides. 
+    // The parameters of a class's constructor will be required upon object
+    // creation outside of the class.
+    public Entry(string date, string promptText, string entryText) {
+        _date = date;
+        _promptText = promptText;
+        _entryText = entryText;
+    }
+
+
     public void DisplayEntry()
     {
         Console.WriteLine($"Date: {_date} - Prompt:");
         Console.WriteLine($"{_entryText}");
+    }
+
+    public void SetDate(string date) {
+        _date = date;
     }
 }
